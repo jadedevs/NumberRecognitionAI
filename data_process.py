38ba856165ee9c60add389d3da3b2a1b84e4aba9
Jade <jadedevofficial@gmail.com>
2023-01-18 20:14:56 +0000

    init

--- data_process.py
+++ data_process.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
-# model expects grayscale 2d images
+# model expects grayscale 2D
 x_train = x_train.reshape((-1, 28, 28, 1))
 x_test = x_test.reshape((-1, 28, 28, 1))
